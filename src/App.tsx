bac832fd34137df6e99b0e049cfead1dbc717540
K.M. Jones-Eastland <kmje405@gmail.com>
2024-03-11 12:52:47 -0600

    Add login page to routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./index.css"
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import { SignUp } from "./components/authentication/SignUpWrapper";
+import LoginPage from "./components/authentication/LoginPage";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/register" element={<SignUp />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
     </>
   )
